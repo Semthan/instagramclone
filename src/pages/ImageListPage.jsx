42a74186776d80953d75ed0b07b9c48d649c6b23
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2020-12-07 09:49:35 +0100

    added fetch() method

--- src/pages/ImageListPage.jsx
+++ src/pages/ImageListPage.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ImageItem from '../components/ImageItem'
-import {imageList} from '../imageList'
+//import {imageList} from '../imageList'
 
 export default function ImageListPage() {
-    console.log(imageList)
-    return (
-        <>  
-          <h2>Instagram</h2> 
-          <div className="row">
-          {imageList.map(imageItem => {
-              return <ImageItem
-                key={imageItem.id}
-                imageItem={imageItem}
-              />
-          })}  
-          </div>
-        </>
-    )
+    
+  const [imageList, setImageList] =  useState([])
+
+  function handleOnClick(){
+    fetch("https://image-mock-data.firebaseio.com/images.json")
+    .then(response => response.json())
+    .then( data => setImageList(data))
+  }
+
+  console.log(imageList)
+  return (
+      <>  
+        <h2>Instagram</h2> 
+        <button onClick={handleOnClick}>Load Data</button>
+        <div className="row">
+        {imageList.map(imageItem => {
+            return <ImageItem
+              key={imageItem.id}
+              imageItem={imageItem}
+            />
+        })}  
+        </div>
+      </>
+  )
 }
